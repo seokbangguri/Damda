764a65b142c056a52f25c57941351b16258c09f3
seokbangguri <34706061+seokbangguri@users.noreply.github.com>
2021-01-12 03:10:20 +0900

    Add files via upload

--- prac1.js
+++ prac1.js
@@ -24,3 +24,13 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
